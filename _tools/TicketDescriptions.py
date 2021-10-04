dcd81ffd60de4c7cb011178fd896539f3dfd98ab
Sinitsaa <naronqaz@gmail.com>
2021-10-04 21:47:12 +0300

    Update TicketDescriptions.py

--- _tools/TicketDescriptions.py
+++ _tools/TicketDescriptions.py
@@ -46,7 +46,9 @@ layer_sex_locks = {"n": ["", ""],
                          " 여성만 가능.",
                          "\nТолько для женских персонажей."]}
 
-ndesc_formats = ["Unlocks a new {itype} for use.{typelock}"]
+ndesc_formats = ["Unlocks a new {itype} for use.{typelock}",
+                "",
+                "Для исп-я азблок-т новый\n{itype}.{typelock}"]}
 
 ntype_locks = {"a": ["All", "", "Все"],
                 "a1": ["Human/Cast Type 1", "", "Человек/CAST (тип1)"],
@@ -125,7 +127,7 @@ def translate_nlayer_desc(item, file_name):
         itype = layered_wear_types[item["tr_text"].split("[", )[1][0:2]][LANG] if item["tr_text"].endswith("]")
                 # Exception for default layered wear since it doesn't have [In], [Ba] etc
                 else layered_wear_types[file_name.split("_")[0][0:2]][LANG],
-        typelock = "" if types == "a" else "\n<yellow>※Type: {0}<c>".format(ntype_locks[types][LANG]),
+        typelock = "" if types == "a" else "\n<yellow>※Type: {0}<c>", "\n<yellow>※Type: {0}<c>", "\n<yellow>※Тип: {0}<c>".format(ntype_locks[types][LANG]),
         hidepanties = "\n<yellow>" + layer_hide_inners[LANG] + "<c>" if hideinner == True else "")
     
     return 0
