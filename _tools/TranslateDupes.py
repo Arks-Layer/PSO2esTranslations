f55c8ea538c66a438c41821a6584f59134c7682c
DabirSA <real.manjackson@gmail.com>
2022-07-27 14:33:56 +0100

    Update to translate descriptions too

--- _tools/TranslateDupes.py
+++ _tools/TranslateDupes.py
@@ -24,18 +24,24 @@ for file_name in file_names:
     print("{0} loaded.".format(file_name))
     items_file.close()
 
-    dupes = 0
+    dupes1 = 0
+    dupes2 = 0
     
     for item in items:
         for item2 in items:
             if (item["jp_text"] == item2["jp_text"]
             and item ["tr_text"] == "" and item2 ["tr_text"] != ""):
                 item["tr_text"] = item2["tr_text"]
-                dupes += 1
+                dupes1 += 1
+            if (item["jp_explain"] == item2["jp_explain"]
+            and item ["tr_explain"] == "" and item2 ["tr_explain"] != ""):
+                item["tr_explain"] = item2["tr_explain"]
+                dupes2 += 1
             continue
         continue
 
-    print("\t{0} duplicate item names translated.".format(dupes))
+    print("\t{0} duplicate item names translated.".format(dupes1))
+    print("\t{0} duplicate item descriptions translated.".format(dupes2))
     
     items_file = codecs.open(os.path.join(json_loc, file_name),
                              mode = 'w', encoding = 'utf-8')
