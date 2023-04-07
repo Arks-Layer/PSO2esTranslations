22f63f83a039de8171df31db9d64d88c6936eafc
hibiki666 <9301774+hibiki666@user.noreply.gitee.com>
2023-04-07 20:39:14 +0800

    CN: Fixes before update

--- _tools/TicketDescriptions.py
+++ _tools/TicketDescriptions.py
@@ -58,7 +58,7 @@ chartable = [
 layered_wear_types = {"In": ["innerwear", "이너웨어", "внутреннюю одежду (In)", "內衣"],
                       "Ba": ["basewear", "베이스웨어", "верхнюю одежду (Ba)", "底衣"],
                       "Se": ["setwear", "세트 웨어", "комплектную одежду (Se)", "套服"],
-                      "Fu": ["full setwear", "풀세트 웨어", "полн.компл.одежду (Fu)", "全身服裝"],
+                      "Fu": ["full setwear", "풀세트 웨어", "полн.компл.одежду (Fu)", "全身套服"],
                       "Ou": ["outerwear", "아우터 웨어", "внешнюю одежду (Ou)", "外套"]}
 
 # Old layered wear format. Must include itype and iname variables.
